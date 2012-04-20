bfd58a173cfebb5d433b7a18fd47d09d3a8b0ac0
Egil Moeller <egil.moller@freecode.no>
2012-04-20 14:35:56 +0200

    Bugfix for recursive dependency-installation and paths

--- hooks.js
+++ hooks.js
@@ -9,23 +9,23 @@ exports.expressConfigure = function(hook_name, args, cb) {
 exports.expressServer = function (hook_name, args, cb) {
   args.app.post('/fileUpload', controller.onRequest);
   args.app.get('/up/:filename(*)', function(req, res) { 
-    var url = req.params['filename'].replace(/\.\./g, '').split("?")[0];
+    var url = req.params.filename.replace(/\.\./g, '').split("?")[0];
     var filePath = path.normalize(path.join(__dirname, "upload", url));
     res.sendfile(filePath, { maxAge: exports.maxAge });
   });
 }
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_fileupload/templates/fileUploadEditbarButtons.ejs");
+    args.content = args.content + eejs.require("ep_fileupload/templates/fileUploadEditbarButtons.ejs", {}, module);
   return cb();
 }
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_fileupload/templates/fileUploadScripts.ejs");
+  args.content = args.content + eejs.require("ep_fileupload/templates/fileUploadScripts.ejs", {}, module);
   return cb();
 }
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_fileupload/templates/fileUploadStyles.ejs");
+  args.content = args.content + eejs.require("ep_fileupload/templates/fileUploadStyles.ejs", {}, module);
   return cb();
 }
