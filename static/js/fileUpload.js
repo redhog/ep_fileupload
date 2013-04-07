a50e020808cd488e1be008ee8463208ab18776c5
John McLear <john@mclear.co.uk>
2013-04-08 00:22:30 +0200

    Comments..

--- static/js/fileUpload.js
+++ static/js/fileUpload.js
@@ -1,14 +1,14 @@
 $(function(){  
     var info = {  
       action: '/fileUpload/',
-      name: 'uploadfile',  
-      onSubmit: function(file, ext){
+      name: 'uploadfile', 
+      onSubmit: function(file, ext){ // On submit we do nothing, it'd be nice to do something but mheh..
       //console.log('Starting...');
       },  
       onComplete: function(file, response){
-        var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-        padeditor.ace.replaceRange(undefined, undefined, " " + response.replace(/^\s+|\s+$/g, '') + " ");
-        padeditor.ace.focus();
+        var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor; // Require the editor..
+        padeditor.ace.replaceRange(undefined, undefined, " " + response.replace(/^\s+|\s+$/g, '') + " "); // Puts the actual URL in the pad..
+        padeditor.ace.focus(); // Put the caret back into the pad
       }  
     }
 
