4b786f8bdd69434eb30a0b627577678afe10fac5
Matthias Wiesner <matthias.wiesner@googlemail.com>
2016-06-02 17:35:41 +0200

    Fix: Extract filePath from response
    In some cases (at least with my etherpad) it happens that the response
    variable already contains schema, domain:port. But we only need the
    filePath starting with "/up".

--- static/js/fileUpload.js
+++ static/js/fileUpload.js
@@ -8,14 +8,14 @@ $(function(){
       onComplete: function(file, response){
         // Require the editor..
         var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-        
+
         // result i.e. "/up/c79133b2c8a97533cc397f8d325ce17a.jpg", trimmed whitespace
-        var filePath = response.replace(/^\s+|\s+$/g, '');
+        var filePath = response.slice(response.indexOf("/up")).trim();
         // "http://example.com/subfolder/p/padID" -> "http://example.com/subfolder"
-        var etherpadRoot = document.location.href.replace(/\/p\/[^\/]+$/,'');
+        var etherpadRoot = document.location.href.slice(0, document.location.href.indexOf("/p/"));
         // "http://example.com/subfolder/up/c79133b2c8a97533cc397f8d325ce17a.jpg"
         var fileUri = etherpadRoot + filePath;
-        
+
         // Puts the actual URL in the pad..
         padeditor.ace.replaceRange(undefined, undefined, " " + fileUri + " ");
         // Put the caret back into the pad
